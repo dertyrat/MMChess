882de4597657db67e9216f3bd94d56b27965f84b
TravisMeares <TravisMeares@gmail.com>
2014-04-14 02:28:30 -0400

    Created simple server that creates threads that continuously listen to clients until a particular exit message is sent

--- src/mmchess/server/testClient/Connection.java
+++ src/mmchess/server/testClient/Connection.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.Observable;
 
 /**
  * @author Travis Meares
+ * Establishes a connection to the server, handles output, and creates/manages
+ * a thread to handle incoming input
  */
-public class Connection extends Observable {
+public class Connection {
     Socket socket;
+    ConnectionThread connectionThread;
     private ObjectInputStream inputStream;
     private ObjectOutputStream outputStream;
 
@@ -24,14 +26,21 @@ public class Connection extends Observable {
             inputStream = new ObjectInputStream(socket.getInputStream());
             outputStream = new ObjectOutputStream(socket.getOutputStream());
 
-            Thread thread = new Thread(new ConnectionThread(inputStream));
+            connectionThread = new ConnectionThread(inputStream);
+            Thread thread = new Thread(connectionThread);
             thread.start();
+            // controller (must implement Observer) will be notified of incoming messages
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Sends a move object to the server using an object stream
+     * @param move the client validated ChessMove object
+     * @return true if move was successful, false if IOException
+     */
     public boolean sendMove(ChessMove move) {
         try {
             outputStream.writeObject(move);
@@ -42,20 +51,11 @@ public class Connection extends Observable {
         return true;
     }
 
-    public ChessMove receiveMove() {
-        ChessMove input = null;
-        try {
-            input = (ChessMove) inputStream.readObject();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } catch (ClassNotFoundException cnfe) {
-            cnfe.printStackTrace();
-        }
-        return input;
-    }
-
+    /**
+     * Close the connection to the socket and end the connection thread
+     */
     public void close() {
-        System.out.printf("Goodbye");
+        connectionThread.end();
         try {
             socket.close();
         } catch (IOException ioe) {
---
+++ src/mmchess/server/testClient/InputHandler.java
@@ -0,0 +1,14 @@
+package mmchess.server.testClient;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by Trav on 4/14/2014.
+ */
+public class InputHandler implements Observer {
+    @Override
+    public void update(Observable o, Object arg) {
+        // figure out input type and handle accordingly
+    }
+}
