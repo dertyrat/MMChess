ecd5753b4a3a18ef62672e48de2ded4a3dd5aa80
Matthew McGuire <dertyrat@gmail.com>
2014-04-27 22:41:40 -0400

    See if this fixes merge issues

--- src/mmchess/client/controller/Controller.java
+++ src/mmchess/client/controller/Controller.java
@@ -20,6 +20,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import mmchess.client.model.Model;
 import mmchess.client.model.Move;
