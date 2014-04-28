10d34e4a23ad9d9c924f84257d3c6631455f9705
TravisMeares <TravisMeares@gmail.com>
2014-04-27 22:21:21 -0400

    Javadoc
    Added javadoc comments to client/server connection classes

--- src/mmchess/server/controller/RunServer.java
+++ src/mmchess/server/controller/RunServer.java
@@ -8,13 +8,17 @@ import java.net.Socket;
  * @author Travis Meares
  */
 public class RunServer {
+    /**
+     * The server main, runs the server until program errors or is closed
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket(8888); // listens for connections
-
         Socket p1, p2;
         Thread matchThread;
         System.out.printf("%s\n", "Waiting for clients....");
-
+        // wait for two connections and initialize a match thread with them
         while (true) {
             p1 = listener.accept();
             System.out.printf("%s\n", "Player 1 connected");
